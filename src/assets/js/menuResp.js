4256f53e4cc3bb8e642051eef51e4120e4eb1941
Yan Ferreira <fabioyan2@gmail.com>
2024-04-22 15:02:25 -0300

    feat: add animation scroll and dropdown menu

---
+++ src/assets/js/menuResp.js
@@ -0,0 +1,10 @@
+$(document).ready(() => {
+  $("#mobile > h3").click(function () {
+    var listaMenu = $("#mobile-list");
+    if (listaMenu.is(":hidden") == true) {
+      listaMenu.slideToggle();
+    } else {
+      listaMenu.slideToggle();
+    }
+  });
+});
---
+++ src/assets/js/menuScroll.js
@@ -0,0 +1,26 @@
+$(document).ready(() => {
+  $("#desktop a").click(function () {
+    var href = $(this).attr("href");
+    var offSetTop = $(href).offset().top;
+
+    $("html,body").animate({ scrollTop: offSetTop }, 1000);
+
+    return false;
+  });
+
+  $("#backToHome").click(function () {
+    let elemento = document.getElementById("top");
+    console.log("clicked");
+    let divScroll = $(elemento).offset().top;
+    $("html,body").animate({ scrollTop: divScroll }, 1000);
+  });
+
+  $("#mobile a").click(function () {
+    var href = $(this).attr("href");
+    var offSetTop = $(href).offset().top;
+
+    $("html,body").animate({ scrollTop: offSetTop }, 1000);
+
+    return false;
+  });
+});
